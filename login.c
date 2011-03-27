d0954833e3a67ab73832b012483d8535c12736ea
Parker Moore <parkrmoore@gmail.com>
2011-03-27 14:06:44 -0400

    initial commit

---
+++ login.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+	printf("%s%c%c\n","Content-Type:text/html;charset=iso-8859-1",13,10);
+	printf("<html>\n<head>\n");
+	printf("\t<title>Logging In...</title>\n<link href='../a4/common.css' rel='stylesheet' type='text/css'>\n\n<body>\n");
+	printf("\t<div id='content'>\n \
+		<span id='heading'>The <span id='teamname'>Quad-Core</span><br />Programmers</span><br /><br /> \
+		This is not implemented yet!<br /><a href='http://cs.mcgill.ca/~vserva1/welcome.html'>Go Home</a>\n<br/> \
+		or go straight to the <a href='http://cs.mcgill.ca/~pcrane/teamPage/'>first room.</a> \
+	</div>\n \
+	<div id='left' class='wing'>&nbsp;</div>\n \
+	<div id='right' class='wing'>&nbsp;</div>\
+	<div id='border'></div>");
+	printf("\n</body>\n</html>");
+	return 0;
+}
