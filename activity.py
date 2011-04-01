d001af2b19a4fee31d54f6b718a98fc1bfbd63de
Parker Moore <parkrmoore@gmail.com>
2011-03-31 23:28:24 -0400

    rewrites changes to inventory.csv

--- activity.py
+++ activity.py
@@ -3,6 +3,9 @@
 import cgi
 
 def check_for_input():
+	f2 = open("inventory.csv", "r")
+	stuff = f2.read().strip().split(", ")
+	f2.close()
 	output = ""
 	form = cgi.FieldStorage()
 	if not form:
@@ -17,6 +20,7 @@ def check_for_input():
 		else:
 			what_i_have += (", "+picked_up)
 		dropped = "nothing"
+		stuff.remove(picked_up)
 	elif form['action'].value == "drop":
 		dropped = str(form["drop"].value)
 		what_i_have = str(form["what_i_have"].value)
@@ -33,12 +37,17 @@ def check_for_input():
 				#the element is last in the list
 				what_i_have = what_i_have.replace(", "+dropped, "")
 		picked_up = "nothing"
+		stuff.append(dropped)
 	else:
 		picked_up = "problem"
 		droppped = "problem"
 		what_i_have = "problem"
 	if what_i_have == "" or what_i_have == " ":
 		what_i_have = "nothing"
+	# write changes to file
+	f2 = open("inventory.csv", "w")
+	f2.write(", ".join(stuff))
+	f2.close()
 	return {'picked_up':picked_up, 'what_i_have': what_i_have, 'dropped': dropped}
 
 from random import choice
@@ -79,10 +88,10 @@ def drop_form(what_i_have):
 def main():
 	try:
 		print "Content-type:text/html\n\n"
-		f2 = open("inventory.csv", "r")
-		stuff = f2.read().strip().split(", ")
 		f1 = open("index.html.pyt", "r")
 		results = check_for_input()
+		f2 = open("inventory.csv", "r")
+		stuff = f2.read().strip().split(", ")
 		pickup_form_stuff = pickup_form(stuff, results["what_i_have"])
 		drop_form_stuff = drop_form(results["what_i_have"])
 		print f1.read() % (pickup_form_stuff, drop_form_stuff, results["what_i_have"], results["picked_up"], results["dropped"])
